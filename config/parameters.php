e387aef8abea71fb4ecc93376b3770ddabe3dc48
Lorenzo José <lorenzojose_96@hotmail.es>
2022-04-07 16:18:01 +0200

    Finally

--- config/parameters.php
+++ config/parameters.php
@@ -1,4 +1,4 @@
 <?php
-define('base_url',"http://localhost/prueba-ingeteam/");
+define('base_url',"/prueba-ingeteam/");
 
 if(isset($_SESSION['login']) && $_SESSION['login']){  include_once 'cpHelper.php';}
